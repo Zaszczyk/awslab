771aadfbb04a487d2778add28602a2def7db73e5
unknown <mateusz.blaszczyk@live.com>
2016-04-12 10:01:12 +0200

    typo

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,39 +1,39 @@
-//stub for lab 1_2
-var AWS = require('aws-sdk');
+	//stub for lab 1_2
+	var AWS = require('aws-sdk');
 
-AWS.config.loadFromPath('./config.json');
+	AWS.config.loadFromPath('./config.json');
 
-var task =  function(request, callback){
+	var task =  function(request, callback){
 
-	var params = {
-  DryRun: true || false,
-  Filters: [
-    {
-      /*Name: 'STRING_VALUE',
-      Values: [
-        'STRING_VALUE',
-         more items */
-      ]
-    },
-    /* more items */
-  ],
-  InstanceIds: [
-   /* 'STRING_VALUE',
-     more items */
-  ],
-  MaxResults: 10,
-  NextToken: 'STRING_VALUE'
-};
+		var params = {
+	  DryRun: true || false,
+	  Filters: [
+	    {
+	      /*Name: 'STRING_VALUE',
+	      Values: [
+	        'STRING_VALUE',
+	         more items 
+	      ]*/
+	    },
+	    /* more items */
+	  ],
+	  InstanceIds: [
+	   /* 'STRING_VALUE',
+	     more items */
+	  ],
+	  MaxResults: 10,
+	  NextToken: 'STRING_VALUE'
+	};
 
-	ec2.describeInstances(params, function(err, data) {
-	  if (err) {
-	  	console.log(err, err.stack)
-	  }
-	  else     {
-	  console.log(data);      
-	  }     // successful response
-	
-	
-}
+		ec2.describeInstances(params, function(err, data) {
+		  if (err) {
+		  	console.log(err, err.stack)
+		  }
+		  else     {
+		  console.log(data);      
+		  }     // successful response
+		
+		
+	}
 
-exports.lab = task
+	exports.lab = task
