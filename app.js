92d3fe85c54e3b6281df7c02a4f30168d4bfd492
unknown <mateusz.blaszczyk@live.com>
2016-04-12 10:15:19 +0200

    last typo?

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-var lab1_1 = require("./lab/lab1_1").lab
-var lab1_2 = require("./lab/lab1_2").lab
+var lab1_1 = require("./lab/lab1_1").lab;
+var lab1_2 = require("./lab/lab1_2").lab;
 var example_1 = require("./example_1").lab;
 
 var PORT = 8080;
 
 
 var urlMap = [
-	{path: "/", action:__dirname + "/static/index.html"},	 
+	{path: "/", action:__dirname + "/static/index.html"},
 	{path: "/digest", action: lab1_1},
-	{path: "/lab1_2", action: lab1_2},	
-	{path: "/example_1", action: example_1}, 
+	{path: "/lab1_2", action: lab1_2},
+	{path: "/example_1", action: example_1},
 	];
 
 var service = require("./lib/service").http(urlMap);
 
 service(PORT);
-
