bef43d58c0130d594b0517cdb25e2f500cb5319d
unknown <mateusz.blaszczyk@live.com>
2016-04-12 10:10:21 +0200

    typo

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,26 +1,25 @@
-	//stub for lab 1_2
-	var AWS = require('aws-sdk');
+//stub for lab 1_2
+var AWS = require('aws-sdk');
 
-	AWS.config.loadFromPath('./config.json');
+AWS.config.loadFromPath('./config.json');
 
-	var task =  function(request, callback){
+var task =  function(request, callback){
 
-		var params = {
-	  DryRun: false,
+	var params = {
+		DryRun: false,
 
-	  MaxResults: 10,
+		MaxResults: 10
 	};
-var ec2 = new AWS.EC2();
+	var ec2 = new AWS.EC2();
 
-		ec2.describeInstances(params, function(err, data) {
-		  if (err) {
-				callback(null, err.stack)
-		  }
-		  else     {
-		  callback(null, data);
-		  }     // successful response
+	ec2.describeInstances(params, function(err, data) {
+		if (err) {
+			callback(null, err.stack)
+		}
+		else     {
+			callback(null, data);
+		}     // successful response
+	)
 
-
-	}
 }
-	exports.lab = task
+exports.lab = task
