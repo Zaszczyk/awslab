4ea9ba7c9305c84e83b6fb262077de75ffce4fd5
unknown <mateusz.blaszczyk@live.com>
2016-04-12 09:57:15 +0200

    details of isntanceS

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -4,6 +4,34 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
+
+	var params = {
+  DryRun: true || false,
+  Filters: [
+    {
+      /*Name: 'STRING_VALUE',
+      Values: [
+        'STRING_VALUE',
+         more items */
+      ]
+    },
+    /* more items */
+  ],
+  InstanceIds: [
+   /* 'STRING_VALUE',
+     more items */
+  ],
+  MaxResults: 10,
+  NextToken: 'STRING_VALUE'
+};
+
+	ec2.describeInstances(params, function(err, data) {
+	  if (err) {
+	  	console.log(err, err.stack)
+	  }
+	  else     {
+	  console.log(data);      
+	  }     // successful response
 	
 	
 }
