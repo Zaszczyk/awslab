9e2d91259d863b4763ebef69a0b9ac55a8207604
unknown <mateusz.blaszczyk@live.com>
2016-04-12 10:16:43 +0200

    another code

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,25 +1,14 @@
-//stub for lab 1_2
 var AWS = require('aws-sdk');
-
-AWS.config.loadFromPath('./config.json');
-
 var task =  function(request, callback){
-
-	var params = {
-		DryRun: false,
-
-		MaxResults: 10
+var params = {
+	  DryRun:  false,
+	  MaxResults: 5
 	};
-	var ec2 = new AWS.EC2();
-
-	ec2.describeInstances(params, function(err, data) {
-		if (err) {
-			callback(null, err.stack)
-		}
-		else     {
-			callback(null, data);
-		}     // successful response
+var ec2 = new AWS.EC2();
+ec2.describeInstances(params, function(err, data) {
+	  if (err) callback(null, err.stack); // an error occurred
+	  else     callback(null,data);           // successful response
 	});
-
 }
+
 exports.lab = task
