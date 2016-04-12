6c5ca636cacbccb6b072456e7f1a085149dcabb9
unknown <mateusz.blaszczyk@live.com>
2016-04-12 10:45:24 +0200

    creating aws

--- lab/init.js
+++ lab/init.js
@@ -1,16 +1,22 @@
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json')
-//AWS.config.update({region:'us-west-2b'});
 var task =  function(request, callback){
-var params = {
-	  DryRun:  false,
-	  MaxResults: 5
+
+	var params = {
+	  ImageId: '1393245935872', /* required */
+	  MaxCount: 10, /* required */
+	  MinCount: 0, /* required */
+	  Monitoring: {
+		Enabled: false /* required */
+	  },
 	};
-var ec2 = new AWS.EC2();
-ec2.describeInstances(params, function(err, data) {
-	  if (err) callback(null, err.stack); // an error occurred
+
+	var ec2 = new AWS.EC2();
+	ec2.runInstances(params, function(err, data) {
+	  if (err)callback(null, err.stack); // an error occurred
 	  else     callback(null,data);           // successful response
 	});
+
 }
 
 exports.lab = task
